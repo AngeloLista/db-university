da786bc3824e5f5f00d61d37863effbc79521c6b
AngeloLista <abstractplayer@gmail.com>
2022-03-10 17:21:08 +0100

    [JOIN] 3rd EX

--- join.sql
+++ join.sql
@@ -2,3 +2,22 @@
 
 
 -- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT * 
+FROM `students`
+JOIN `degrees`
+ON `degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
+
+-- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT *
+FROM `degrees`
+JOIN `departments`
+ON `department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di neuroscienze'
+
+-- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)`
+SELECT *
+FROM `course_teacher`
+JOIN `courses`
+ON `course_id` = `courses`.`id`
+WHERE `teacher_id` = 44
