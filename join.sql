13dd4fcb08a9457bbd7569c9f33699f7142547da
AngeloLista <abstractplayer@gmail.com>
2022-03-10 18:08:31 +0100

    [JOIN] 5th

--- join.sql
+++ join.sql
@@ -41,3 +41,16 @@ ON `course_id` = `courses`.`id`
 JOIN `teachers`
 ON `teacher_id` = `teachers`.`id`
 ORDER BY `courses`.`name` ASC
+
+-- Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT *
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica'
